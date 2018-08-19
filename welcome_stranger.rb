705f4cc5fc93fa5f69792dd94f884a190ad35798
Oshi99 <oshiryaeva@protonmail.com>
2018-08-19 18:32:26 -0500

    some changes

--- welcome_stranger.rb
+++ welcome_stranger.rb
@@ -1,5 +1,3 @@
 def greetings(arr, hsh)
   "Hello, #{arr.join(' ')}! Nice to have a #{hsh[:title]} #{hsh[:occupation]} around."
 end
-
-p greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
